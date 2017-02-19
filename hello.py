75a916286efcc455879047d7483be08c6619de84
Zlmrfi <Zlmrfi@gmail.com>
2017-02-19 20:20:24 +0500

    f

---
+++ hello.py
@@ -0,0 +1,15 @@
+def app(environ,start_response):
+    s=''
+    s=environ["QUERY_STRING"]
+    otto=[]
+    if s:
+        ot="200 OK"
+        s=s[s.find('?')+1:]
+        while s:
+            ore=s.find('&')
+            if ore==-1:
+                ore=len(s)
+            otto.append(bytes(s[:ore]+'\n'))
+            s=s[ore+1:]
+    start_response('200 OK', [("Content-Type", "text/plain")])
+    return otto
